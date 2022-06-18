cd134362118742df1087ff92efcf5c4ce3aadc01
HristijanSlavkoski <kiko_slavkoski@yahoo.com>
2022-06-18 13:17:17 +0200

    Add css to register

--- TheatreMvc/app/views/register.php
+++ TheatreMvc/app/views/register.php
@@ -1,8 +1,14 @@
-<form
-                id="register-form"
-                method="POST"
-                action="<?php  ?>https://localhost/TheatreMvc/TheatreMvc/public/home/register"
-                >
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
+        <title>Register</title>
+    </head>
+
+    <body>
+        <form id="register-form" method="POST" action="<?php  ?>https://localhost/TheatreMvc/TheatreMvc/public/home/register">
             <input type="text" placeholder="Username *" name="username">
             <span class="invalidFeedback">
                 <?php echo $data['usernameError']; ?>
@@ -22,8 +28,8 @@
             <span class="invalidFeedback">
                 <?php echo $data['confirmPasswordError']; ?>
             </span>
-
-            <button id="submit" type="submit" value="submit">Submit</button>
-
-        
+            <footer>Already a member? <a href="login">Login here</a></footer>
+            <button id="submit" type="submit" value="submit">Submit</button> 
         </form>
+    </body>
+</html>
