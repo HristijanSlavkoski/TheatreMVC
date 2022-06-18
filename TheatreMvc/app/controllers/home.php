d3f8bbab8cfa7127bf36247688d6bd7ee1805b8b
HristijanSlavkoski <kiko_slavkoski@yahoo.com>
2022-06-18 14:35:03 +0200

    Add login feature

--- TheatreMvc/app/controllers/home.php
+++ TheatreMvc/app/controllers/home.php
@@ -1,9 +1,9 @@
 <?php
 class Home extends Controller
 {
-
     public function __construct() {
         $this->userModel = $this->model('User');
+        $this->db=new Database;
     }
     
     //da probame da kreirame nekoja akcija
@@ -14,7 +14,6 @@ class Home extends Controller
          $user->name=$name;
   
          $this->view('home/index',['name'=>$user->name]);
-
     }   
 
     public function login()
@@ -27,13 +26,46 @@ class Home extends Controller
             'passwordError' => ''
         ];
 
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            // Process form
+            // Sanitize POST data
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+    
+            $data = [
+                'username' => trim($_POST['username']),
+                'password' => trim($_POST['password']),
+                'usernameError' => '',
+                'passwordError' => ''
+            ];
+            //Validate username on letters/numbers
+            if (empty($data['username'])) {
+                $data['usernameError'] = 'Please enter username.';
+            }
 
-        $this->view('home/login',$data);
+            // Validate password on length, numeric values,
+            if(empty($data['password'])){
+                $data['passwordError'] = 'Please enter password.';
+            } 
+
+            // Make sure that errors are empty
+            if (empty($data['usernameError']) && empty($data['passwordError'])) {
+                $this->db->query('SELECT password FROM user WHERE username = :username');
+                //Username param will be binded with the username variable
+                $this->db->bind(':username', $data['username']);
+                $result = $this->db->resultSet();
+                $userPassword = array_column($result, 'password');
+                if(password_verify($data['password'],$userPassword[0])){
+                    header('location: ' . URLROOT . '/public/home/index/done');
+                } else {
+                    die('Wrong password');
+                }
+            }
+        }
+        $this->view('home/login', $data);
     }
     public function register()
-    {
-             
-         $data = [
+    {     
+        $data = [
             'username' => '',
             'email' => '',
             'password' => '',
@@ -48,73 +80,73 @@ class Home extends Controller
             // Sanitize POST data
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
     
-                  $data = [
-                    'username' => trim($_POST['username']),
-                    'email' => trim($_POST['email']),
-                    'password' => trim($_POST['password']),
-                    'confirmPassword' => trim($_POST['confirmPassword']),
-                    'usernameError' => '',
-                    'emailError' => '',
-                    'passwordError' => '',
-                    'confirmPasswordError' => ''
-                ];
+            $data = [
+                'username' => trim($_POST['username']),
+                'email' => trim($_POST['email']),
+                'password' => trim($_POST['password']),
+                'confirmPassword' => trim($_POST['confirmPassword']),
+                'usernameError' => '',
+                'emailError' => '',
+                'passwordError' => '',
+                'confirmPasswordError' => ''
+            ];
     
-                $nameValidation = "/^[a-zA-Z0-9]*$/";
-                $passwordValidation = "/^(.{0,7}|[^a-z]*|[^\d]*)$/i";
-    
-                //Validate username on letters/numbers
-                if (empty($data['username'])) {
-                    $data['usernameError'] = 'Please enter username.';
-                } elseif (!preg_match($nameValidation, $data['username'])) {
-                    $data['usernameError'] = 'Name can only contain letters and numbers.';
-                }
-    
-                //Validate email
-                if (empty($data['email'])) {
-                    $data['emailError'] = 'Please enter email address.';
-                } elseif (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
-                    $data['emailError'] = 'Please enter the correct format.';
-                } else {
-                    //Check if email exists.
-                    if ($this->userModel->findUserByEmail($data['email'])) {
-                    $data['emailError'] = 'Email is already taken.';
-                    }
-                }
-    
-               // Validate password on length, numeric values,
-                if(empty($data['password'])){
-                  $data['passwordError'] = 'Please enter password.';
-                } elseif(strlen($data['password']) < 6){
-                  $data['passwordError'] = 'Password must be at least 8 characters';
-                } elseif (preg_match($passwordValidation, $data['password'])) {
-                  $data['passwordError'] = 'Password must be have at least one numeric value.';
-                }
-    
-                //Validate confirm password
-                 if (empty($data['confirmPassword'])) {
-                    $data['confirmPasswordError'] = 'Please enter password.';
-                } else {
-                    if ($data['password'] != $data['confirmPassword']) {
-                    $data['confirmPasswordError'] = 'Passwords do not match, please try again.';
-                    }
-                }
-    
-                // Make sure that errors are empty
-                if (empty($data['usernameError']) && empty($data['emailError']) && empty($data['passwordError']) && empty($data['confirmPasswordError'])) {
-    
-                    // Hash password
-                    $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
-    
-                    //Register user from model function
-                    if ($this->userModel->register($data)) {
-                        //Redirect to the login page
-                        header('location: ' . URLROOT . '/public/home/login');
-                    } else {
-                        die('Something went wrong.');
-                    }
+            $nameValidation = "/^[a-zA-Z0-9]*$/";
+            $passwordValidation = "/^(.{0,7}|[^a-z]*|[^\d]*)$/i";
+
+            //Validate username on letters/numbers
+            if (empty($data['username'])) {
+                $data['usernameError'] = 'Please enter username.';
+            } elseif (!preg_match($nameValidation, $data['username'])) {
+                $data['usernameError'] = 'Name can only contain letters and numbers.';
+            }
+
+            //Validate email
+            if (empty($data['email'])) {
+                $data['emailError'] = 'Please enter email address.';
+            } elseif (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+                $data['emailError'] = 'Please enter the correct format.';
+            } else {
+                //Check if email exists.
+                if ($this->userModel->findUserByEmail($data['email'])) {
+                $data['emailError'] = 'Email is already taken.';
                 }
             }
-            $this->view('register', $data);
+
+            // Validate password on length, numeric values,
+            if(empty($data['password'])){
+                $data['passwordError'] = 'Please enter password.';
+            } elseif(strlen($data['password']) < 6){
+                $data['passwordError'] = 'Password must be at least 8 characters';
+            } elseif (preg_match($passwordValidation, $data['password'])) {
+                $data['passwordError'] = 'Password must be have at least one numeric value.';
+            }
+
+            //Validate confirm password
+                if (empty($data['confirmPassword'])) {
+                $data['confirmPasswordError'] = 'Please enter password.';
+            } else {
+                if ($data['password'] != $data['confirmPassword']) {
+                $data['confirmPasswordError'] = 'Passwords do not match, please try again.';
+                }
+            }
+
+            // Make sure that errors are empty
+            if (empty($data['usernameError']) && empty($data['emailError']) && empty($data['passwordError']) && empty($data['confirmPasswordError'])) {
+
+                // Hash password
+                $hash = password_hash($data['password'], PASSWORD_DEFAULT);
+                $data['password'] = $hash;
+
+                //Register user from model function
+                if ($this->userModel->register($data)) {
+                    //Redirect to the login page
+                    header('location: ' . URLROOT . '/public/home/login');
+                } else {
+                    die('Something went wrong.');
+                }
+            }
         }
-          
+        $this->view('register', $data);
+    }   
 }
--- TheatreMvc/app/views/home/login.php
+++ TheatreMvc/app/views/home/login.php
@@ -1,17 +1,28 @@
-<?php echo $data['title'];
-?>
-
-<form action="<?php echo URLROOT; ?>/app/controllers/home" method ="POST">
-<input type="text" placeholder="Username *" name="username">
-<span class="invalidFeedback">
-    <?php echo $data['usernameError']; ?>
-</span>
-
-<input type="password" placeholder="Password *" name="password">
-<span class="invalidFeedback">
-    <?php echo $data['passwordError']; ?>
-</span>
-
-<button id="submit" type="submit" value="submit">Submit</button>
-
-</form>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://www.phptutorial.net/app/css/style.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"">
+        <title>Login</title>
+    </head>
+    <body>
+        <?php echo $data['title'];?>
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-4">
+                <form id="login-form" method="POST" action="<?php  ?>https://localhost/TheatreMVC/TheatreMvc/public/home/login">
+                    <input type="text" placeholder="Username *" name="username">
+                    <span class="invalidFeedback">
+                        <?php echo $data['usernameError']; ?>
+                    </span>
+                    <input type="password" placeholder="Password *" name="password">
+                    <span class="invalidFeedback">
+                        <?php echo $data['passwordError']; ?>
+                    </span>
+                    <button id="submit" type="submit" value="submit">Submit</button>
+                </form>    
+            </div>
+        </div>
+    </body>
+</html>
