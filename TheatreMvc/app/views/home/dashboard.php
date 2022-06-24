9f72677ecc831af37cb00ccfb9ac2b2fc59f77bd
HristijanSlavkoski <kiko_slavkoski@yahoo.com>
2022-06-24 15:49:10 +0200

    Add google map on dashbaord page

--- TheatreMvc/app/views/home/dashboard.php
+++ TheatreMvc/app/views/home/dashboard.php
@@ -8,9 +8,14 @@
          require APPROOT . '/views/includes/navigation.php';
       ?>
       <div class="list">
-         <div>
+         <div class="dashboardText">
             Добредојдовте на официјалната страна на театарот во Прилеп
          </div>    
+      </div>
+      <div class="map">      
+         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11981.983554096267!2d21.5584255!3d41.3415741!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe376fd5f0918a856!2z0KLQtdCw0YLQsNGAICLQktCe0IjQlNCQ0J0g0KfQldCg0J3QntCU0KDQmNCd0KHQmtCYIiAo0KHQu9GD0LbQsdC10L0g0JLQu9C10Lcp!5e0!3m2!1sen!2smk!4v1656077429670!5m2!1sen!2smk" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            Контакт број: 077-123-456 <br>
+            Маил: vojdanCernodrinski@teatar.mk
       </div>
    </body>
 </html>
